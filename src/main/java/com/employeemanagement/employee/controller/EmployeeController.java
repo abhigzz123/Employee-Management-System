9bb2bf07e29e7871a091338abb97e6753736b546
abhigyanzemoso <abhigyan.pandey@zemosolabs.com>
2022-03-27 13:42:26 +0530

    Add-new-employee

--- src/main/java/com/employeemanagement/employee/controller/EmployeeController.java
+++ src/main/java/com/employeemanagement/employee/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.employeemanagement.employee.controller;
 
+import com.employeemanagement.employee.entity.Employee;
 import com.employeemanagement.employee.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -19,5 +22,17 @@ public class EmployeeController {
         return "index";
     }
 
+    @GetMapping("/addNewEmployee")
+    public String addNewEmployeeForm(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "newEmployeeForm";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
 
 }
